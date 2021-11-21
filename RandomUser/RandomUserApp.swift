a6db0cf6462dfc972b622a76aab066b37da95593
Noah Brauner <nbbrauner@gmail.com>
2021-11-21 08:29:19 -0500

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Noah Brauner on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
